14dadc1509e6b9ea4eb1d457fc6120f3b481e819
Christian Oliveros <christianol_01@hotmail.com>
2021-03-31 21:27:44 +0200

    Models Placeholders

---
+++ models.py
@@ -0,0 +1,13 @@
+
+
+def stanfordModel():
+    return None
+
+def stepOneModel(stanfordModel):
+    return None
+
+def opticalFlowModel():
+    return None
+
+def twoStreamsModel(oneModel, flowModel):
+    return None
