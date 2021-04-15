b85b1a4a0ca0980663ffd490f9afc21ec2388030
vupers <ubermatte@gmail.com>
2021-04-15 16:28:43 +0200

    right function

--- models.py
+++ models.py
@@ -45,15 +45,18 @@ def cyclicalLRate(
 
 
 # function for creating a naive inception block
-def inception_module(layer_in, f1, f2, f3):
+def inception_module(layer_in, f1, f2_in, f2_out, f3_in, f3_out, f4_out):
     # 1x1 conv
-    conv1 = Conv2D(f1, (1, 1), padding="same", activation="relu")(layer_in)
+    conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(layer_in)
     # 3x3 conv
-    conv3 = Conv2D(f2, (3, 3), padding="same", activation="relu")(layer_in)
+    conv3 = Conv2D(f2_in, (1,1), padding='same', activation='relu')(layer_in)
+    conv3 = Conv2D(f2_out, (3,3), padding='same', activation='relu')(conv3)
     # 5x5 conv
-    conv5 = Conv2D(f3, (5, 5), padding="same", activation="relu")(layer_in)
+    conv5 = Conv2D(f3_in, (1,1), padding='same', activation='relu')(layer_in)
+    conv5 = Conv2D(f3_out, (5,5), padding='same', activation='relu')(conv5)
     # 3x3 max pooling
-    pool = MaxPooling2D((3, 3), strides=(1, 1), padding="same")(layer_in)
+    pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(layer_in)
+    pool = Conv2D(f4_out, (1,1), padding='same', activation='relu')(pool)
     # concatenate filters, assumes filters/channels last
     layer_out = concatenate([conv1, conv3, conv5, pool], axis=-1)
     return layer_out
